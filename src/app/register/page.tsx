838e8e603c0a78d6859f7ba72aba2cb5bf1e603c
Rogel Ramirez Cabreja <rogelprogrammer@gmail.com>
2025-03-26 23:36:08 -0400

    Fixes

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -9,42 +9,102 @@ import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import { useState } from "react";
 
-const [formData, setFormData] = useState({
-  ci: "",
-  name: "",
-  lastname1: "",
-  lastname2: "",
-  cellphone1: "",
-  cellphon2: "",
-  address: "",
-  email: "",
-  username: "",
-  password: "",
-  type: "Cliente",
-});
-
 export default function Login() {
+  const [formData, setFormData] = useState({
+    ci: "",
+    name: "",
+    lastname1: "",
+    lastname2: "",
+    cellphone1: "",
+    cellphon2: "",
+    address: "",
+    email: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    type: "Cliente",
+  });
   return (
     <div className="flex h-full w-full min-h-[calc(100vh-88px)] items-center justify-center p-4 bg-[#e7e8e9]">
       <div className="flex flex-col bg-white w-120 rounded-lg shadow-lg p-6 space-y-6">
         <h3 className="text-center text-3xl font-bold text-[#022953]">
           Registrarse
         </h3>
-        <Input placeholder="Carnet de identidad" />
+        <Input
+          placeholder="Carnet de identidad"
+          value={formData.ci}
+          onChange={(e) => setFormData({ ...formData, ci: e.target.value })}
+        />
         <div className="flex space-x-4">
-          <Input placeholder="Nombre" />
-          <Input placeholder="Primer apellido" />
-          <Input placeholder="Segundo apellido" />
+          <Input
+            placeholder="Nombre"
+            value={formData.name}
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+          />
+          <Input
+            placeholder="Primer apellido"
+            value={formData.lastname1}
+            onChange={(e) =>
+              setFormData({ ...formData, lastname1: e.target.value })
+            }
+          />
+          <Input
+            placeholder="Segundo apellido"
+            value={formData.lastname2}
+            onChange={(e) =>
+              setFormData({ ...formData, lastname2: e.target.value })
+            }
+          />
         </div>
         <div className="flex space-x-4">
-          <Input placeholder="Celular 1" />
-          <Input placeholder="Celular 2 (opcional)" />
+          <Input
+            placeholder="Celular 1"
+            value={formData.cellphone1}
+            onChange={(e) =>
+              setFormData({ ...formData, cellphone1: e.target.value })
+            }
+          />
+          <Input
+            placeholder="Celular 2 (opcional)"
+            value={formData.cellphon2}
+            onChange={(e) =>
+              setFormData({ ...formData, cellphon2: e.target.value })
+            }
+          />
         </div>
-        <Input placeholder="Dirección" />
-        <Input placeholder="Correo electrónico" />
-        <Input placeholder="Nombre de usuario" />
-        <Input placeholder="Contraseña" />
-        <Input placeholder="Confirmar contraseña" />
+        <Input
+          placeholder="Dirección"
+          value={formData.address}
+          onChange={(e) =>
+            setFormData({ ...formData, address: e.target.value })
+          }
+        />
+        <Input
+          placeholder="Correo electrónico"
+          value={formData.email}
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+        />
+        <Input
+          placeholder="Nombre de usuario"
+          value={formData.username}
+          onChange={(e) =>
+            setFormData({ ...formData, username: e.target.value })
+          }
+        />
+        <Input
+          placeholder="Contraseña"
+          value={formData.password}
+          onChange={(e) =>
+            setFormData({ ...formData, password: e.target.value })
+          }
+        />
+        <Input
+          placeholder="Confirmar contraseña"
+          value={formData.confirmPassword}
+          onChange={(e) =>
+            setFormData({ ...formData, confirmPassword: e.target.value })
+          }
+        />
         <Button className="w-full bg-[#022953] hover:bg-[#034078] hover:shadow-lg">
           Registrarse
         </Button>
